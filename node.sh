e02b6284c3cb82ebd608cb160005ec8398f7ab0e
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-23 22:41:48 +0300

    Update

--- node.sh
+++ node.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-BUILD=0
+OPTIONS_BUILD=0
 
 for option in "${@}"; do
   case "${option}" in
     -b|--build)
-      BUILD=1
+      OPTIONS_BUILD=1
       ;;
   esac
 done
@@ -17,7 +17,7 @@ for option in "${@}"; do
 
       curl -sSL https://get.docker.io/ubuntu/ | sudo bash
 
-      if [ "${BUILD}" -eq 1 ]; then
+      if [ "${OPTIONS_BUILD}" -eq 1 ]; then
         sudo docker build -t simpledrupalcloud/node .
       else
         sudo docker pull simpledrupalcloud/node
@@ -34,7 +34,7 @@ for option in "${@}"; do
 
       git clone http://git.simpledrupalcloud.com/simpledrupalcloud/docker-node.git $CONTEXT
 
-      if [ "${BUILD}" -eq 1 ]; then
+      if [ "${OPTIONS_BUILD}" -eq 1 ]; then
         $CONTEXT/node.sh -b install
       else
         $CONTEXT/node.sh install
