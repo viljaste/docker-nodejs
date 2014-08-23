e5e154d89671684cf1fe2577c824b123df2e085c
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-23 21:33:55 +0300

    Update

---
+++ node.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+case "$1" in
+  install)
+    sudo apt-get install -y curl
+
+    curl -sSL https://get.docker.io/ubuntu/ | sudo bash
+
+    sudo docker build -t fenomen .
+
+    sudo apt-get install -y realpath
+
+    sudo cp $(realpath -s $0) /usr/local/bin/node
+    ;;
+  update)
+    sudo docker rmi fenomen
+
+    CONTEXT=$(mktemp  -d)
+
+    git clone http://git.simpledrupalcloud.com/simpledrupalcloud/docker-node.git $CONTEXT
+
+    $CONTEXT/node.sh install
+    ;;
+  *)
+    sudo docker run --rm -i -t -v $(pwd):/app node "${@}"
+    ;;
+esac
