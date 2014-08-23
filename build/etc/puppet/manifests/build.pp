4d1c0b8e839194b5302c4918e0b55681879211e0
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-23 20:47:41 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -3,23 +3,17 @@ node default {
     ensure => present,
     source => '/tmp/build/run.sh',
     mode => 755
-  }
-
+  } ->
   file { '/etc/puppet/manifests/run.pp':
     ensure => present,
     source => '/tmp/build/etc/puppet/manifests/run.pp'
-  }
-
+  } ->
   package { 'curl':
     ensure => present
-  }
-
+  } ->
   exec { 'curl -sL https://deb.nodesource.com/setup | bash -':
-    require => Package['curl']
-  }
-
+  } ->
   package { 'nodejs':
-    ensure => present,
-    require => Exec['curl -sL https://deb.nodesource.com/setup | bash -']
+    ensure => present
   }
 }
