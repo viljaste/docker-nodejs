2373de1c6d75e67e82c3cb3078e36e4fb7c9034f
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-24 18:36:35 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -12,12 +12,12 @@ node default {
     require => Exec['/usr/bin/apt-get update']
   }
 
-  exec { '/usr/bin/curl -sL https://deb.nodesource.com/setup | bash -':
+  exec { '/bin/bash -c "curl -sL https://deb.nodesource.com/setup | bash -"':
     require => Package['curl']
   }
 
   package { 'nodejs':
     ensure => present,
-    require => Exec['/usr/bin/curl -sL https://deb.nodesource.com/setup | bash -']
+    require => Exec['/bin/bash -c "curl -sL https://deb.nodesource.com/setup | bash -"']
   }
 }
